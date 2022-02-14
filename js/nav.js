9b90893d1bbbeca3204f46b1fda584edc4520cb4
santi_moyano <smoyano.di@gmail.com>
2022-02-14 17:03:53 -0300

    redes-navbar

--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,18 @@
 let primaryNav = document.querySelector ('.primary-nav');
-// console.log(primaryNav);
+let primaryHead = document.querySelector ('.primary-header');
 let menuToggle = document.querySelector ('.menu-toggle');
-// console.log(menuToggle)
 
 menuToggle.addEventListener('click', () => {
     let visible = primaryNav.getAttribute('data-visible');
 
     if (visible === "false") {
         primaryNav.setAttribute('data-visible', true);
+        primaryHead.setAttribute('aria-expanded', true);
         menuToggle.setAttribute('aria-expanded', true)
 
     } else {
         primaryNav.setAttribute('data-visible', false);
+        primaryHead.setAttribute('aria-expanded', false);
         menuToggle.setAttribute('aria-expanded', false)
     };
 });
